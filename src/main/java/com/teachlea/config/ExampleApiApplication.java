d5613ab9ab8d4b4abe8a3e6f2f1f626dfe902d9f
rajeksingh13 <rajeksingh2@gmail.com>
2022-07-17 18:22:02 +0530

    Quarkus Open Api

---
+++ src/main/java/com/teachlea/config/ExampleApiApplication.java
@@ -0,0 +1,23 @@
+package com.teachlea.config;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import javax.ws.rs.core.Application;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title="Example API",
+                version = "1.0.1",
+                contact = @Contact(
+                        name = "Example API Support",
+                        url = "http://teachlea.com/contact",
+                        email = "teachlea.com"),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
+)
+public class ExampleApiApplication extends Application {
+}
