84a03b3f283b407c88cc6ed51ab91cde4b480a18
rajeksingh13 <rajeksingh2@gmail.com>
2022-08-04 20:19:47 +0530

    Jib Maven Plugin Demo with Spring Boot Application

---
+++ src/main/java/com/teachlea/mavenjibdemo/MavenJibDemoApplication.java
@@ -0,0 +1,13 @@
+package com.teachlea.mavenjibdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJibDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJibDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachlea/mavenjibdemo/MavenJibDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachlea.mavenjibdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJibDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
