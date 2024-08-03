3ceae2864fb9e1bb53e18b1a1506a077338a5387
rajeksingh13 <rajeksingh2@gmail.com>
2024-08-03 23:43:29 +0530

    Demo for Spring AI

---
+++ src/test/java/com/teachlea/springAI/spring_ai_demo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachlea.springAI.spring_ai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
